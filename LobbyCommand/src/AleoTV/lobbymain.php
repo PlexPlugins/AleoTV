3885b5f4ffb191e5f42c3b46780d60c557f591e8
AleoTV <53917303+AleoTV@users.noreply.github.com>
2020-02-13 01:47:13 +0100

    update lobbymain.php

--- LobbyCommand/src/AleoTV/lobbymain.php
+++ LobbyCommand/src/AleoTV/lobbymain.php
@@ -3,10 +3,30 @@
 namespace AleoTV;
 
 
-
 use pocketmine\Server;
 use pocketmine\Player;
-use pocketmine\utils\TextFormat as C;
 use pocketmine\plugin\PluginBase;
+
+use pocketmine\command\{Command, CommandSender};
+
+class Main extends PluginBase {
+
+public funtion onEnable(){
+}
+
+public funtion on command(CommandSender $player, Command $cmd, string $label, array $args):bool {
+
+switch($cmd->get name()){
+case "discord":
+if ($player instanceof Player){
+     $player->sendMessage("https://discord.gg/ExUPBYe");
+}
+break;
+}
+
+  return true;
+}  )
+
+
 
 
