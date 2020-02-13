b0203995d5d71c26b9e5e0cb937887c2139982b7
AleoTV <53917303+AleoTV@users.noreply.github.com>
2020-02-13 12:55:48 +0100

    Rename lobbymain.php to main.php

---
+++ LobbyCommand/src/AleoTV/main.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace AleoTV;
+
+
+use pocketmine\Server;
+use pocketmine\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\{Command, CommandSender};
+
+class lobbymain extends PluginBase implements Listener {
+ 
+ 
+ 
+} 
+
+   
+  
