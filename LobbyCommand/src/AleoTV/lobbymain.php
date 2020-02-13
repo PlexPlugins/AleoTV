28f5721baa5039109f86cf6a648cfddb0b77ae59
AleoTV <53917303+AleoTV@users.noreply.github.com>
2020-02-13 02:10:23 +0100

    update lobbymain.php

--- LobbyCommand/src/AleoTV/lobbymain.php
+++ LobbyCommand/src/AleoTV/lobbymain.php
@@ -6,10 +6,12 @@ namespace AleoTV;
 use pocketmine\Server;
 use pocketmine\Player;
 use pocketmine\plugin\PluginBase;
-
 use pocketmine\command\{Command, CommandSender};
 
 
-
-
-
+class main extends PluginBase {
+ public $prefix = "LobbyCommand"
+     public function onEnable() {
+          $this->getLogger()->info($this->prefix . "Plugin wurde aktiviert");
+     }
+} 
